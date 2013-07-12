5553ccdb293ba12a789161ab083e30bc6bf2f76f
David Bromberg <bromberg@labri.fr>
2013-07-12 12:11:25 +0200

    update RBOP spec and GossipPush

---
+++ src/main/java/fr/labri/starnet/policies/rbop/Identifier.java
@@ -0,0 +1,27 @@
+package fr.labri.starnet.policies.rbop;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import fr.labri.starnet.Address;
+import fr.labri.starnet.INode;
+import fr.labri.starnet.Message;
+import fr.labri.starnet.policies.commons.CommonVar;
+import fr.labri.starnet.policies.commons.NeighborGraph;
+import fr.labri.timedautomata.ITimedAutomata.SpawnAdapter;
+
+public class Identifier extends SpawnAdapter<INode> {
+	@Override
+	public String getSpawnerKey(INode context, String parentKey) {
+		Message m = (Message)context.getStorage().get(CommonVar.CURRENT_MESSAGE);
+		String id = ""+m.getMessageID();
+		NeighborGraph ng = (NeighborGraph) context.getStorage().get(RBOPVar.NEIGHBOR_GRAPH);
+		Collection<Address> ca = ng.getReceivedNeighbors(m);
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		hm.put(CommonVar.CURRENT_MESSAGE, m);
+		hm.put(RBOPVar.NEIGHBOR_GRAPH_RECEIVED, ca);
+		context.getStorage().put(id, hm);
+		return id;
+		
+	}
+}
--- src/main/java/fr/labri/starnet/policies/rbop/RBOPVar.java
+++ src/main/java/fr/labri/starnet/policies/rbop/RBOPVar.java
@@ -2,4 +2,5 @@ package fr.labri.starnet.policies.rbop;
 
 public class RBOPVar {
 	public static final String NEIGHBOR_GRAPH = "neighbor_graph";
+	public static final String NEIGHBOR_GRAPH_RECEIVED = "neighbor_graph_received";
 }
