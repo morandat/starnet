562749069b925d3a73cb37a3c54843912c992090
jbourcie <Johann.Bourcier@irisa.fr>
2013-07-12 17:53:02 +0200

    Add the Prim and kruskal algorithm
    Merge branch 'master' of https://github.com/davidb987654/starnet
    
    Conflicts:
    	src/main/java/fr/labri/starnet/policies/rbop/ProcessMstActions.java

--- src/main/java/fr/labri/starnet/policies/commons/NeighborGraph.java
+++ src/main/java/fr/labri/starnet/policies/commons/NeighborGraph.java
@@ -1,14 +1,17 @@
 package fr.labri.starnet.policies.commons;
 
+import com.sun.javafx.collections.transformation.SortedList;
+import edu.uci.ics.jung.algorithms.shortestpath.PrimMinimumSpanningTree;
 import edu.uci.ics.jung.graph.DirectedSparseGraph;
+import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.util.Pair;
 import fr.labri.starnet.Address;
 import fr.labri.starnet.Message;
 import fr.labri.starnet.Position;
+import org.apache.commons.collections15.Factory;
+import org.apache.commons.collections15.Transformer;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created with IntelliJ IDEA.
@@ -21,9 +24,9 @@ public class NeighborGraph {
 
     private GraphNode currentNode;
 
-    private DirectedSparseGraph<GraphNode, GraphEdge> neighborGraph;
+    private Graph<GraphNode, GraphEdge> neighborGraph;
 
-    public static NeighborGraph createNeighborGraph(DirectedSparseGraph<GraphNode, GraphEdge> neighborGraph, GraphNode gn){
+    public static NeighborGraph createNeighborGraph(Graph<GraphNode, GraphEdge> neighborGraph, GraphNode gn){
         for (GraphNode gn1 : neighborGraph.getVertices()){
             if (gn1.getAddress().asInt() == gn.getAddress().asInt()){
                 return new NeighborGraph(gn1, neighborGraph);
@@ -33,7 +36,7 @@ public class NeighborGraph {
 
     }
 
-    private NeighborGraph(GraphNode gn, DirectedSparseGraph<GraphNode, GraphEdge> neighborGraph){
+    private NeighborGraph(GraphNode gn, Graph<GraphNode, GraphEdge> neighborGraph){
         this.neighborGraph = neighborGraph;
         this.currentNode = gn;
 
@@ -139,4 +142,180 @@ public class NeighborGraph {
          }
         return true;
     }
+
+    public void transformPrim(){
+        PrimMinimumSpanningTree<GraphNode, GraphEdge> pmst = new PrimMinimumSpanningTree<GraphNode, GraphEdge>(
+        new Factory<DirectedSparseGraph<GraphNode, GraphEdge>>() {
+            @Override
+            public DirectedSparseGraph<GraphNode, GraphEdge> create() {
+                return new DirectedSparseGraph<GraphNode, GraphEdge>();  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        }, new Transformer<GraphEdge, Double>() {
+            @Override
+            public Double transform(GraphEdge graphEdge) {
+                return graphEdge.getPower();
+            }
+        }
+        );
+
+        neighborGraph = pmst.transform(neighborGraph);
+        addAllReverseEdge();
+    }
+
+    private void addAllReverseEdge(){
+        ArrayList<GrahEdgeWithNodes> toAdd = new ArrayList<GrahEdgeWithNodes>();
+        for (GraphEdge ge : neighborGraph.getEdges()){
+            GraphNode sourceNode = neighborGraph.getSource(ge);
+            GraphNode destNode = neighborGraph.getDest(ge);
+            toAdd.add(new GrahEdgeWithNodes(new GraphEdge(ge.getPower(), ge.getDistance()), destNode, sourceNode));
+        }
+
+        for (GrahEdgeWithNodes gewn : toAdd){
+            neighborGraph.addEdge(gewn.getEdge(), gewn.getSourceNode(), gewn.getDestNode());
+        }
+    }
+
+
+
+
+    public void transformKruskal() {
+
+        // List of edges sorted
+        SortedSet<GraphEdge> edges = new TreeSet<GraphEdge>(new Comparator<GraphEdge>() {
+             @Override
+             public int compare(GraphEdge o1, GraphEdge o2) {
+                 GraphEdge ge1 = (GraphEdge)o1;
+                 GraphEdge ge2 = (GraphEdge) o2;
+                 return Double.compare(ge1.getPower(), ge2.getPower());
+             }
+
+             @Override
+             public boolean equals(Object obj) {
+                 return false;
+             }
+         });
+
+        // Collections giving the set of connex nodes
+        Collection<Collection<GraphNode>> connexCompo = new ArrayList<Collection<GraphNode>>();
+
+        // Kruskal Tree
+        Graph<GraphNode, GraphEdge> kruskalTree = new DirectedSparseGraph<GraphNode, GraphEdge>();
+
+        GraphNode sourceNode;
+        GraphNode extNode;
+        GraphEdge currentEdge;
+
+        //edges init
+        edges.addAll(neighborGraph.getEdges());
+
+        // tree init
+        for (GraphNode gn : neighborGraph.getVertices()){
+            kruskalTree.addVertex(gn);
+        }
+
+        // connexCompo initialisation
+        Collection<GraphNode> ensSommet = neighborGraph.getVertices();
+        for (GraphNode somCourant : ensSommet) {
+            Collection<GraphNode> temp = new ArrayList<GraphNode>();
+            temp.add(somCourant);
+            connexCompo.add(temp);
+        }
+        // boucle qui trouve les aretes de l'arbre
+        while (connexCompo.size() > 1 && !edges.isEmpty()) {
+            currentEdge = edges.first();
+            // weight
+            sourceNode = (GraphNode) neighborGraph.getSource(currentEdge);
+            extNode = (GraphNode) neighborGraph.getDest(currentEdge);
+
+            if (!belongsToConnexCompo(connexCompo, sourceNode, extNode)) {
+                fusionComposanteConnexe(connexCompo, sourceNode, extNode);
+
+
+                kruskalTree.addEdge(new GraphEdge(currentEdge.getPower(), currentEdge.getDistance()), neighborGraph.getSource(currentEdge), neighborGraph.getDest(currentEdge));
+                kruskalTree.addEdge(new GraphEdge(currentEdge.getPower(), currentEdge.getDistance()), neighborGraph.getDest(currentEdge), neighborGraph.getSource(currentEdge));
+            }
+            edges.remove(currentEdge);
+
+        }
+
+        neighborGraph = kruskalTree;
+    }
+
+    private boolean belongsToConnexCompo(Collection<Collection<GraphNode>> connexCompo, GraphNode sourceNode, GraphNode destNode) {
+
+        for (Collection<GraphNode> graphNodeCollection : connexCompo){
+            boolean find1 = false, find2 = false;
+            for (GraphNode gn : graphNodeCollection){
+                if (gn.getAddress().asInt() == sourceNode.getAddress().asInt()){
+                    find1 = true;
+                }
+                if (gn.getAddress().asInt() == destNode.getAddress().asInt()){
+                    find2 = true;
+                }
+                if (find1 && find2){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * fusionne les deux composants connexes de somOri et somExt qui sont
+     * distinctes
+     *
+     * @param sourceNode
+     *            Ã©quivalent du x
+     * @param destNode
+     *            Ã©quivalent du y
+     */
+    private void fusionComposanteConnexe(Collection<Collection<GraphNode>> connexCompo, GraphNode sourceNode, GraphNode destNode) {
+
+        Collection<GraphNode> sourceGraphNodes = null;
+        Collection<GraphNode> destGraphNodes = null;
+        for (Collection<GraphNode> graphNodes : connexCompo){
+            for (GraphNode gn : graphNodes){
+                if (gn.getAddress().asInt() == sourceNode.getAddress().asInt()){
+                    sourceGraphNodes = graphNodes;
+                }
+                if (gn.getAddress().asInt() == destNode.getAddress().asInt()){
+                    destGraphNodes = graphNodes;
+                }
+                if (sourceGraphNodes!=null && destGraphNodes!=null){
+                    break;
+                }
+            }
+        }
+
+        if (sourceGraphNodes != null && destGraphNodes != null){
+            sourceGraphNodes.addAll(destGraphNodes);
+            connexCompo.remove(destGraphNodes);
+        }
+    }
+
+
+    private class GrahEdgeWithNodes {
+        private GraphEdge edge;
+        private GraphNode sourceNode;
+        private GraphNode destNode;
+
+        public GrahEdgeWithNodes(GraphEdge edge, GraphNode source, GraphNode dest){
+            this.edge = edge;
+            this. sourceNode = source;
+            this.destNode = dest;
+        }
+
+        public GraphEdge getEdge(){
+            return this.edge;
+        }
+
+        public GraphNode getSourceNode () {
+            return this.sourceNode;
+        }
+
+        public GraphNode getDestNode(){
+            return this.destNode;
+        }
+    }
+
 }
--- src/main/java/fr/labri/timedautomata/AutomataViewer.java
+++ src/main/java/fr/labri/timedautomata/AutomataViewer.java
@@ -6,7 +6,7 @@ public class AutomataViewer {
 	public static JFrame viewAsFrame(TimedAutomata<?> automata) {
 		JFrame frame = new JFrame("Simple Graph View");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().add(automata.asPanel()); 
+		//frame.getContentPane().add(automata.asPanel());
 		frame.pack();
 		frame.setVisible(true); 
 		return frame;
